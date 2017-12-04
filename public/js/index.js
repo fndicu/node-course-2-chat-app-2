98a988adb03c6189a6a71b5edb4d51ed6e42f7a8
fndicu <fnidcu@hubspot.com>
2017-12-03 23:18:31 -0500

    Add form for messages and show incoming messages on the screen

--- public/js/index.js
+++ public/js/index.js
@@ -10,5 +10,21 @@
             });
 
         socket.on('newMessage', function (message){
-    console.log('New message', message);
+             console.log('newMessage', message);
+             var li = $('<li></li>');
+             li.text(`${message.from}: ${message.text}`);
+             $('#messages').append(li)
+});
+
+
+
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'user',
+        text: $('[name=message]').val()
+    }, function () {
+
+    });
 });
