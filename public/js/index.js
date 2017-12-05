fd215f1158136c210e76c4856e984f82cd74c9cc
fndicu <fnidcu@hubspot.com>
2017-12-04 23:58:02 -0500

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -27,14 +27,16 @@ socket.on('newLocationMessage', function(message){
 
 });
 
+
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
+    var messageTextbox = $('[name=message]');
 
     socket.emit('createMessage', {
         from: 'user',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('')
     });
 });
 
@@ -45,13 +47,17 @@ locationButton.on('click', function(){
         return alert('Geolocation no supported by your browser')
     } 
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
         console.log(position);
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
