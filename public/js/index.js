4f8020a1a27ff58294cdca4506d6cf7e0b7cb748
fndicu <fnidcu@hubspot.com>
2017-12-10 18:46:55 -0500

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,40 @@
  var socket = io();
 
-            socket.on('connect', function () {
-                console.log('connected to server');
+function scrollToBottom (){
+    //selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child')
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop =  messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
 
-            });
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight)
+    }
+}
+socket.on('connect', function () {
+    console.log('connected to server');
 
-            socket.on('disconnect', function () {
-                console.log('disconnected from Server')
-            });
+});
 
-        socket.on('newMessage', function (message){
-            var formattedTime = moment(message.createdAt).format('h:mm a')
-            var template = $('#message-template').html();
-            var html = Mustache.render(template, {
-                text: message.text,
-                from: message.from,
-                createdAt: formattedTime
-            });
+socket.on('disconnect', function () {
+    console.log('disconnected from Server')
+});
 
-            $('#messages').append(html)
+socket.on('newMessage', function (message){
+    var formattedTime = moment(message.createdAt).format('h:mm a')
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -30,6 +46,7 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 
